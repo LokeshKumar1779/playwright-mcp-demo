dc38fb332562e545f4dfa5b2ce85b1531c9625d8
LokeshKumar1779 <LokeshKumar1779@users.noreply.github.com>
2026-04-24 18:13:36 +0000

    deploy: 26504458862670e3d67e68efabbcd85d22afe354

--- tests/dashboard.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "../fixtures/login.fixture";
-import { expect } from "playwright/test";
-
-test("should login", async ({ loginPage }) => {
-  await loginPage.login(process.env.LOGIN_USER!, process.env.LOGIN_PWD!);
-  await expect(loginPage.page).toHaveURL(
-    "https://www.saucedemo.com/inventory.html",
-  );
-});
--- tests/video.spec.ts
+++
@@ -1,7 +0,0 @@
-/* import { expect, test } from "@playwright/test";
-
-test("video record test", async ({ page }) => {
-  await page.goto("https://www.amazon.com");
-  await expect(page).toHaveTitle("/Amazon/");
-});
- */
